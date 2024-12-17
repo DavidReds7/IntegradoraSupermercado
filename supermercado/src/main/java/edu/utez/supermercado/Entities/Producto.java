5ec0e165662c265af4082805ac32036996c5e0b1
DavidReds7 <20233tn152@utez.edu.mx>
2024-12-17 13:17:08 -0600

    commit 22

--- supermercado/src/main/java/edu/utez/supermercado/Entities/Producto.java
+++ supermercado/src/main/java/edu/utez/supermercado/Entities/Producto.java
@@ -15,24 +15,31 @@ public class Producto {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public Double getPrecio() {
         return precio;
     }
+
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
+
     public Integer getStock() {
         return stock;
     }
+    
     public void setStock(Integer stock) {
         this.stock = stock;
     }
