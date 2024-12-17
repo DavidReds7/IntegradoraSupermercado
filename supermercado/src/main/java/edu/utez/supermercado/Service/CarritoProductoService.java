f3b33ed0b40461a3c2571e23d150fa7f4709ec3a
DavidReds7 <20233tn152@utez.edu.mx>
2024-12-17 13:07:18 -0600

    commit 15

--- supermercado/src/main/java/edu/utez/supermercado/Service/CarritoProductoService.java
+++ supermercado/src/main/java/edu/utez/supermercado/Service/CarritoProductoService.java
@@ -1,5 +1,30 @@
-/*package edu.utez.supermercado.Service;
+package edu.utez.supermercado.Service;
 
-public class CarritoService {
+import edu.utez.supermercado.Entities.CarritoProducto;
+import edu.utez.supermercado.Repository.CarritoProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-}*/
+@Service
+public class CarritoProductoService {
+
+    @Autowired
+    private CarritoProductoRepository carritoProductoRepository;
+
+    public CarritoProducto agregarProductoACarrito(CarritoProducto carritoProducto) {
+        return carritoProductoRepository.save(carritoProducto);
+    }
+
+    public List<CarritoProducto> obtenerProductosDelCarrito(Long clienteId) {
+        return carritoProductoRepository.findByClienteId(clienteId);
+    }
+
+    public void eliminarProductoDelCarrito(Long id) {
+        carritoProductoRepository.deleteById(id);
+    }
+
+    public void deshacerEliminacionProducto(CarritoProducto carritoProducto) {
+        carritoProductoRepository.save(carritoProducto);
+    }
+}
